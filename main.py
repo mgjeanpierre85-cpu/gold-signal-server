82fae450a268c45a18e5b8c6821d92ee5d331412
mgjeanpierre85-cpu <mgjeanpierre85@gmail.com>
2026-02-05 18:45:50 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
+
+@app.route("/signal", methods=["POST"])
+def signal():
+    data = request.json
+    print("Signal recibido:", data)
+    return jsonify({"status": "ok"}), 200
+
+if __name__ == "__main__":
+    # Render usa el puerto asignado por la variable PORT
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
