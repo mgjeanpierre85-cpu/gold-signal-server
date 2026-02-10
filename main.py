0b764499954289678f90a307a420698350cda712
mgjeanpierre85-cpu <mgjeanpierre85@gmail.com>
2026-02-10 08:53:43 -0600

    Update main.py

--- main.py
+++ main.py
@@ -162,13 +162,13 @@ def predict():
 
     msg = (
         "🚨 <b>~ ML Signal ~</b>🤖\n\n"
-        f"<b>Pair:</b>           {ticker}\n"
-        f"<b>Direction:</b>    {direction}\n"
-        f"<b>Entry:</b>          {open_price:.2f}\n"
-        f"<b>SL:</b>              {sl:.2f}\n"
-        f"<b>TP:</b>              {tp:.2f}\n"
-        f"<b>TF:</b>              {timeframe}m\n"
-        f"<b>Date:</b>           {date_formatted}"
+        f"📊<b>Pair:</b>           {ticker}\n"
+        f"↕️<b>Direction:</b>    {direction}\n"
+        f"💵<b>Entry:</b>          {open_price:.2f}\n"
+        f"🛑<b>SL:</b>              {sl:.2f}\n"
+        f"✅<b>TP:</b>              {tp:.2f}\n"
+        f"⏰<b>TF:</b>              {timeframe}m\n"
+        f"📅<b>Date:</b>          {date_formatted}"
     )
 
     send_telegram(msg)
